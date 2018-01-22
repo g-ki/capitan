d9fff95960e3dffd6435be979d588c40332e351c
Georgi Kiryakov <george.kiryakov@gmail.com>
2018-01-22 03:07:49 +0200

    create droplet at DO

--- capitan/blueprints/nodes.py
+++ capitan/blueprints/nodes.py
@@ -1,6 +1,8 @@
+import time
 from flask import Blueprint, request, session, g, redirect, url_for, abort, \
      render_template, flash, current_app
 from capitan.app import docker_client
+import digitalocean
 
 url_prefix = '/nodes'
 bp = Blueprint('nodes', __name__)
@@ -30,6 +32,19 @@ def new():
     token = docker_client.swarm.attrs['JoinTokens']['Worker']
     address = docker_client.info()['Swarm']['RemoteManagers'][0]['Addr']
     script = f"docker swarm join --token {token} {address}"
+
+    if 'DOCEAN_TOKEN' in current_app.config:
+        time_id = int(time.time())
+        droplet = digitalocean.Droplet(
+            token=current_app.config['DOCEAN_TOKEN'],
+            name=f'worker-{time_id}',
+            region='fra1', # Frankfurt 1
+            image='ubuntu-16-04-x64', # Ubuntu 16.04 x64
+            size_slug='s-1vcpu-1gb',  # 1vcpu + 1gb
+            ssh_keys=[], # TODO: use ssh keys
+            user_data=None # TODO: write cloud-init script
+        )
+        # droplet.create()
 
     return render_template('nodes/new.html', script=script)
 
